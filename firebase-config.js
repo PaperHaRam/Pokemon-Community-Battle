0eae821fabdc61af05837291736aca69b2d01d23
PaperHaRam <dao00o@naver.com>
2026-09-14 21:07:46 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBxJu9-s2ASD0EbS8Z0BAzEau-cdv1_22k",
+  authDomain: "pokemon-community-battle.firebaseapp.com",
+  databaseURL: "https://pokemon-community-battle-default-rtdb.firebaseio.com/",
+  projectId: "pokemon-community-battle",
+  storageBucket: "pokemon-community-battle.firebasestorage.app",
+  messagingSenderId: "906380323268",
+  appId: "1:906380323268:web:5fe5775512576270349c91"
+};
